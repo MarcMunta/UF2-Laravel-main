241e2f24f1a8423c129f9b91bfa656fc22ee2f07
Marc Muntané <mmclara1050@gmail.com>
2025-04-13 21:27:36 +0200

    M

--- app/Models/Film.php
+++ app/Models/Film.php
@@ -2,19 +2,36 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Film extends Model
 {
-    use HasFactory; 
-
-    protected $table = 'films';
+    use HasFactory;
 
     protected $fillable = [
-        'title',
-        'genre',
+        'name',
         'year',
-        'county',
+        'genre',
+        'country',
+        'duration',
+        'img_url',
+        'created_at',
+        'updated_at',
     ];
+    protected $casts = [
+        'name' => 'string',
+        'year' => 'integer',
+        'genre' => 'string',
+        'country' => 'string',
+        'duration' => 'integer',
+        'img_url' => 'string',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    public function Actors()
+    {
+        return $this->belongsToMany(Actor::class);
+    }
 }
