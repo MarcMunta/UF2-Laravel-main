5b67b7589f360632f3c34d690c1df0c493a6563e
Marc Muntané <mmclara1050@gmail.com>
2025-05-14 18:53:32 +0200

    .

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/films', [FilmController::class, 'listFilmsWithActors']);  
-Route::get('/films/{id}', [FilmController::class, 'isFilm']);          
-Route::post('/films', [FilmController::class, 'createFilm']);          
-Route::put('/films/{id}', [FilmController::class, 'update']);          
-Route::delete('/films/{id}', [FilmController::class, 'destroy']);      
+Route::get('/films', [FilmController::class, 'listFilmsWithActors']);
+Route::get('/films/{id}', [FilmController::class, 'isFilm']);
+Route::post('/films', [FilmController::class, 'createFilm']);
+Route::put('/films/{id}', [FilmController::class, 'update']);
+Route::delete('/films/{id}', [FilmController::class, 'destroy']);
 
-Route::get('/actors', [ActorController::class, 'index']);              
-Route::get('/actors/{id}', [ActorController::class, 'show']);          
-Route::post('/actors', [ActorController::class, 'create']);           
-Route::put('/actors/{id}', [ActorController::class, 'update']);        
-Route::delete('/actors/{id}', [ActorController::class, 'destroy']);    
+Route::get('/actors', [ActorController::class, 'index']);
+Route::get('/actors/{id}', [ActorController::class, 'show']);
+Route::post('/actors', [ActorController::class, 'create']);
+Route::put('/actors/{id}', [ActorController::class, 'update']);
+Route::delete('/actors/{id}', [ActorController::class, 'destroy']);
