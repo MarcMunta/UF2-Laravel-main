cdec2ba54508f0a4e48f2a39210630d811d3ce3c
Marc Muntané <mmclara1050@gmail.com>
2025-05-14 11:55:11 +0200

    Refactor database migrations: remove old films and actor_film migrations, create new films migration, and update actor_film migration with foreign key constraints.

--- database/migrations/2025_04_13_194017_create_actor_film_table.php
+++ database/migrations/2025_04_13_194017_create_actor_film_table.php
@@ -6,20 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('actor_film', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('actor_id');
+            $table->unsignedBigInteger('film_id');
             $table->timestamps();
+
+            $table->foreign('actor_id')->references('id')->on('actors')->onDelete('cascade');
+            $table->foreign('film_id')->references('id')->on('films')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('actor_film');
